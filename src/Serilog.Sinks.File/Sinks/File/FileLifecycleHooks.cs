4c8c3d4868b2b495464d395e67ac1279b3778b1e
C. Augusto Proiete <augusto@proiete.com>
2020-11-08 23:49:32 -0400

    Add ability to capture path of log file opened, via FileLifecycleHooks

--- src/Serilog.Sinks.File/Sinks/File/FileLifecycleHooks.cs
+++ src/Serilog.Sinks.File/Sinks/File/FileLifecycleHooks.cs
@@ -32,6 +32,21 @@ namespace Serilog.Sinks.File
         /// A value must be returned from overrides of this method. Serilog will flush and/or dispose the returned value, but will not
         /// dispose the stream initially passed in unless it is itself returned.
         /// </remarks>
+        /// <param name="path">The full path to the log file.</param>
+        /// <param name="underlyingStream">The underlying <see cref="Stream"/> opened on the log file.</param>
+        /// <param name="encoding">The encoding to use when reading/writing to the stream.</param>
+        /// <returns>The <see cref="Stream"/> Serilog should use when writing events to the log file.</returns>
+        public virtual Stream OnFileOpened(string path, Stream underlyingStream, Encoding encoding) => OnFileOpened(underlyingStream, encoding);
+
+        /// <summary>
+        /// Initialize or wrap the <paramref name="underlyingStream"/> opened on the log file. This can be used to write
+        /// file headers, or wrap the stream in another that adds buffering, compression, encryption, etc. The underlying
+        /// file may or may not be empty when this method is called.
+        /// </summary>
+        /// <remarks>
+        /// A value must be returned from overrides of this method. Serilog will flush and/or dispose the returned value, but will not
+        /// dispose the stream initially passed in unless it is itself returned.
+        /// </remarks>
         /// <param name="underlyingStream">The underlying <see cref="Stream"/> opened on the log file.</param>
         /// <param name="encoding">The encoding to use when reading/writing to the stream.</param>
         /// <returns>The <see cref="Stream"/> Serilog should use when writing events to the log file.</returns>
