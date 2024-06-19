4688ab35dfa882e838063ef97bafdae3f064709d
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-06-19 21:32:02 +1000

    Dependency updates, langversion, embed README

--- example/Sample/Program.cs
+++ example/Sample/Program.cs
@@ -1,35 +1,27 @@
 using Serilog;
 using Serilog.Debugging;
 
-namespace Sample;
+SelfLog.Enable(Console.Out);
 
-public class Program
+var sw = System.Diagnostics.Stopwatch.StartNew();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("log.txt")
+    .CreateLogger();
+
+for (var i = 0; i < 1000000; ++i)
 {
-    public static void Main(string[] args)
-    {
-        SelfLog.Enable(Console.Out);
-
-        var sw = System.Diagnostics.Stopwatch.StartNew();
-
-        Log.Logger = new LoggerConfiguration()
-            .WriteTo.File("log.txt")
-            .CreateLogger();
-
-        for (var i = 0; i < 1000000; ++i)
-        {
-            Log.Information("Hello, file logger!");
-        }
-
-        Log.CloseAndFlush();
-
-        sw.Stop();
-
-        Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds} ms");
-        Console.WriteLine($"Size: {new FileInfo("log.txt").Length}");
-
-        Console.WriteLine("Press any key to delete the temporary log file...");
-        Console.ReadKey(true);
-
-        File.Delete("log.txt");
-    }
+    Log.Information("Hello, file logger!");
 }
+
+Log.CloseAndFlush();
+
+sw.Stop();
+
+Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds} ms");
+Console.WriteLine($"Size: {new FileInfo("log.txt").Length}");
+
+Console.WriteLine("Press any key to delete the temporary log file...");
+Console.ReadKey(true);
+
+File.Delete("log.txt");
--- test/Serilog.Sinks.File.Tests/Support/Extensions.cs
+++ test/Serilog.Sinks.File.Tests/Support/Extensions.cs
@@ -4,7 +4,7 @@ namespace Serilog.Sinks.File.Tests.Support;
 
 public static class Extensions
 {
-    public static object LiteralValue(this LogEventPropertyValue @this)
+    public static object? LiteralValue(this LogEventPropertyValue @this)
     {
         return ((ScalarValue)@this).Value;
     }
